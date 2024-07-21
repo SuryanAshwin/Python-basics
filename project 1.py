17313a7f6fbfbd364e850713f63a937b91a4fd49
SuryanAshwin <suryanashwin1029@gmail.com>
2024-07-21 19:35:11 +0530

    Add files via upload

---
+++ project 1.py
@@ -0,0 +1,19 @@
+"""color=str(input("enter the color"))
+if(color=="red"):
+    print("stop")
+elif(color=="yellow"):
+    print("get ready")
+elif(color=="green"):
+    print("go")
+else:
+    print("type a valid colour")"""
+
+std=int(input('Enter your class'))
+if((class>1))and((class<=5)):
+    print("He/She belongs to primary")
+    else:
+if((class>6))and((class<=8)):
+    print("He/She belongs to secondary")
+    else:
+if((class>9))and((class<=12)):
+    print("He/She belongs to Senior")
---
+++ project 2.py
@@ -0,0 +1,4 @@
+for m in range(6,13):
+    for d in range(1,11):
+        print(m,'*' ,d, '=')
+        print("__________________")
